022cdcc5d8df891e47b840e9cd611327f97c560b
30106 <>
2023-04-05 01:04:30 +0000

    and, or

---
+++ and.c
@@ -0,0 +1,9 @@
+// #include <stdio.h>
+// int main(){
+//   int a=15;
+//   int b=20;
+//   int c=a&b;
+//   printf("AND연산 결과: %d",c);
+//   return 0;
+// }
+// //00001111 00010100 일치하는게 2^2자리=> c=4
---
+++ or.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  int a=15;
+  int b=20;
+  int c=a|b;
+  printf("OR연산 결과: %d",c);
+  return 0;
+}
+//00001111 00010100 
